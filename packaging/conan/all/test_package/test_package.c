419c43f303a3ff277be13d42e2725a3833e46c05
Oleksandr <barbarik012001@gmail.com>
2026-09-21 17:11:57 +0300

    🗑️Remove package templates from repository

--- packaging/conan/all/test_package/test_package.c
+++
@@ -1,9 +0,0 @@
-#include <mhook-lib/mhook.h>
-
-static BOOL (*volatile set_hook)(PVOID*, PVOID) = &Mhook_SetHook;
-static BOOL (*volatile unhook)(PVOID*) = &Mhook_Unhook;
-
-int main(void)
-{
-    return (set_hook && unhook) ? 0 : 1;
-}
